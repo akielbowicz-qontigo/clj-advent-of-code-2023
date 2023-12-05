bf4dae9b93be52c75740f0843f992f033250bc3c
akielbowicz <akielbowicz@QONTIGO.com>
2023-12-05 18:15:36 -0300

    Agrego el test e implementacion de prueba del dia 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -4,3 +4,12 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+
+
+(defn dia-1
+  "resuelve el problema del dia 1"
+  [input]
+  "A")
+   
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -5,3 +5,13 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(def input-simple "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet")
+
+(deftest test-dia-1
+  (testing "Simple input"
+    (is (= (dia-1 input-simple) 77)))
+  )
