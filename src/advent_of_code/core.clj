d86685fb2eadafa9f9bab5f6caaccfa07942a39e
Augusto Kielbowicz <79214493+akielbowicz-qontigo@users.noreply.github.com>
2023-12-14 21:59:05 +0000

    solucion del dia 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,12 +1,21 @@
 (ns advent-of-code.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn primero-y-ultimo
+  [digitos]
+  [(first digitos) (last digitos)])
+
+(defn digitos
+  [linea]
+  (filter #(Character/isDigit %) linea))
+
+(defn procesar-linea
+  [linea]
+  (Integer/parseInt (clojure.string/join (primero-y-ultimo (digitos linea)))))
 
 (defn dia-1
   "resuelve el problema del dia 1"
   [input]
-  "100")
+  (apply + 
+  (map procesar-linea (clojure.string/split-lines input))
+  ))
    
--- test/advent_of_code/core_test.clj
+++ test/advent_of_code/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [advent-of-code.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
-
 (def input-simple "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
@@ -13,5 +9,5 @@ treb7uchet")
 
 (deftest test-dia-1
   (testing "Simple input"
-    (is (= (dia-1 input-simple) 77)))
+    (is (= (dia-1 input-simple) 142)))
   )
